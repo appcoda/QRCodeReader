efa0da4243326a46f2b52a57d730a24d043ddf62
StephenClarkApps <contact.stephenclark@gmail.com>
2018-05-04 03:48:48 +0100

    Fix getting capture device.

--- QRCodeReader/QRScannerController.swift
+++ QRCodeReader/QRScannerController.swift
@@ -37,9 +37,7 @@ class QRScannerController: UIViewController {
         super.viewDidLoad()
 
         // Get the back-facing camera for capturing videos
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
-        
-        guard let captureDevice = deviceDiscoverySession.devices.first else {
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
             print("Failed to get the camera device")
             return
         }
