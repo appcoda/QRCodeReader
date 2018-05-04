fea4d7153a4e47abd71c7ecb402419e73d74e2d4
Stephen Clark <contact.stephenclark@gmail.com>
2018-05-04 04:06:56 +0100

    Orientation of image view fixed to update with device rotation.

--- QRCodeReader/QRScannerController.swift
+++ QRCodeReader/QRScannerController.swift
@@ -118,6 +118,40 @@ class QRScannerController: UIViewController {
         
         present(alertPrompt, animated: true, completion: nil)
     }
+  private func updatePreviewLayer(layer: AVCaptureConnection, orientation: AVCaptureVideoOrientation) {
+    layer.videoOrientation = orientation
+    videoPreviewLayer?.frame = self.view.bounds
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    
+    if let connection =  self.videoPreviewLayer?.connection  {
+      let currentDevice: UIDevice = UIDevice.current
+      let orientation: UIDeviceOrientation = currentDevice.orientation
+      let previewLayerConnection : AVCaptureConnection = connection
+      
+      if previewLayerConnection.isVideoOrientationSupported {
+        switch (orientation) {
+        case .portrait:
+          updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+          break
+        case .landscapeRight:
+          updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeLeft)
+          break
+        case .landscapeLeft:
+          updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeRight)
+          break
+        case .portraitUpsideDown:
+          updatePreviewLayer(layer: previewLayerConnection, orientation: .portraitUpsideDown)
+          break
+        default:
+          updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+          break
+        }
+      }
+    }
+  }
 
 }
 
